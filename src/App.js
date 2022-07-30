c872485a3231f71d79c5dab4f9385c810fed53bd
Max Kravchenko <59091609+Helltis@users.noreply.github.com>
2022-07-30 14:41:37 +0300

    working on product gallery

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React, { Component } from "react";
 // import Product from "./components/Product";
 // import Category from "./containers/Category";
 // import PDP from "./pages/PDP";
-import ProductProperty from "./components/ProductProperty";
+// import ProductProperty from "./components/ProductProperty";
+// import ProductTitle from "./components/ProductTitle";
+import ProductGallery from "./components/ProductGallery";
+// import ProductColor from "./components/ProductColor";
 import "./App.css";
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <ProductProperty />
+        <ProductGallery />
       </div>
     );
   }
--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import product from "../assets/product.png";
+import product2 from "../assets/product2.png";
+import product3 from "../assets/product3.png";
+import product4 from "../assets/product4.png";
 import "./productGallery.scss";
 
 export class ProductGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.selectedImg = this.arrImg[0];
+  }
+
+  arrImg = [product, product2, product3, product4];
+  // const [selectedImg, setSelectedImg] = useState(arrImg[0]);
   render() {
     return (
       <div className="productGallery">
-        {/* <a className="productGallery_thumbnail" href="#thumb"></a> */}
+        <div className="productGallery_container">
+          <img
+            src={this.selectedImg}
+            alt="Selected"
+            className="productGallery_image"
+          />
+        </div>
+        <div className="productGallery_thumbs">
+          {this.arrImg.map((img, index) => (
+            <img
+              src={img}
+              alt="dog"
+              key={index}
+              onClick={() => this.setSelectedImg(img)}
+            />
+          ))}
+        </div>
       </div>
     );
   }
