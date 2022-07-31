cc54709c2c2bdfe27a85f9c0f1a0593bb85be2aa
Helltis <59091609+Helltis@users.noreply.github.com>
2022-07-31 14:16:37 +0300

    finished product gallery component

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -6,32 +6,30 @@ import product4 from "../assets/product4.png";
 import "./productGallery.scss";
 
 export class ProductGallery extends Component {
-  constructor(props) {
-    super(props);
-    this.selectedImg = this.arrImg[0];
-  }
-
   arrImg = [product, product2, product3, product4];
-  // const [selectedImg, setSelectedImg] = useState(arrImg[0]);
+  state = {
+    selectedImg: this.arrImg[0],
+  };
+
   render() {
     return (
       <div className="productGallery">
-        <div className="productGallery_container">
-          <img
-            src={this.selectedImg}
-            alt="Selected"
-            className="productGallery_image"
-          />
-        </div>
         <div className="productGallery_thumbs">
           {this.arrImg.map((img, index) => (
             <img
               src={img}
               alt="dog"
               key={index}
-              onClick={() => this.setSelectedImg(img)}
+              onClick={() => this.setState({ selectedImg: img })}
             />
           ))}
+        </div>
+        <div className="productGallery_container">
+          <img
+            src={this.state.selectedImg}
+            alt="Selected"
+            className="productGallery_image"
+          />
         </div>
       </div>
     );
