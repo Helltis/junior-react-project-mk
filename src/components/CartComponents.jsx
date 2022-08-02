54eedd6ebcfcca320ecc3ce44f405037dd1ddf6d
Helltis <59091609+Helltis@users.noreply.github.com>
2022-08-02 19:40:30 +0300

    finished cart layout

--- src/components/CartComponents.jsx
+++ src/components/CartComponents.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import minus from "../assets/minus-square.svg";
+import plus from "../assets/plus-square.svg";
+import left from "../assets/arrow-left.svg";
+import right from "../assets/arrow-right.svg";
 import "./cartComponents.scss";
 import product from "../assets/product.png";
 import product2 from "../assets/product2.png";
@@ -8,28 +12,24 @@ import product4 from "../assets/product4.png";
 export class CartComponents extends Component {
   arrImg = [product, product2, product3, product4];
   state = {
-    quantity: 0,
+    quantity: 1,
     image: this.arrImg[0],
   };
   render() {
     return (
       <div className="cartComponents">
         <div className="cartComponents_quantity">
-          <button className="cartComponents_quantity_increase">+</button>
+          <input type="image" src={plus} alt="plus" />
           <p className="cartComponents_quantity_number">
             {this.state.quantity}
           </p>
-          <button className="cartComponents_quantity_decrease">-</button>
+          <input type="image" src={minus} alt="minus" />
         </div>
         <div className="cartComponents_preview">
-          <img
-            src={this.state.image}
-            alt="product preview"
-            className="cartComponents_preview_image"
-          />
+          <img src={this.state.image} alt="product preview" />
           <div className="cartComponents_preview_buttons">
-            <button className="cartComponents_preview_left">{"<"}</button>
-            <button className="cartComponents_preview_right">{">"}</button>
+            <input type="image" src={left} alt="left" />
+            <input type="image" src={right} alt="right" />
           </div>
         </div>
       </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
-import { Navbar } from "../containers/Navbar";
+import CartComponents from "../components/CartComponents";
+import Navbar from "../components/Navbar";
+import { ProductTitle } from "../components/ProductTitle";
+import { ProductProperty } from "../components/ProductProperty";
+import { ProductColor } from "../components/ProductColor";
+import "./cart.scss";
 
 export class Cart extends Component {
   render() {
     return (
       <div className="pageCart">
         <Navbar />
+        <p className="pageCart_title">CART</p>
+        <span className="pageCart_divider" />
+        <div className="pageCart_item">
+          <div className="pageCart_selectedProps">
+            <ProductTitle />
+            <span>$50.00</span>
+            <ProductProperty />
+            <ProductColor />
+          </div>
+          <CartComponents />
+        </div>
+        <span className="pageCart_divider" />
+        <div className="pageCart_item">
+          <div className="pageCart_selectedProps">
+            <ProductTitle />
+            <ProductProperty />
+            <ProductColor />
+          </div>
+          <CartComponents />
+        </div>
       </div>
     );
   }
